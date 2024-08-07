0df6653a2dd290c67ff6d5a51c873db6ff3c49ce
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2024-08-07 14:28:24 +0500

    One coding example as well.

---
+++ Python Complete/master_if_cond.py
@@ -0,0 +1,13 @@
+def main():
+    x, y = 8, 8
+    if (x > y):
+        st = "x is greater than y"
+    elif (x < y):
+        st = "x is less than y"
+    else:
+        st = "x and y are same"
+    print(st)
+    return
+
+if __name__ == '__main__':
+    main()
