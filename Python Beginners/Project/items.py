a317ea8b0b311aa9ce0173560468ba71fad57905
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2024-08-15 21:18:11 +0500

    I wrote all the items inside the items.py

---
+++ Python Beginners/Project/items.py
@@ -0,0 +1,5 @@
+categories = {"fruits", "vegetables", "detergents", "grocery", "deserts", "clothes", "medicine"}
+fruits = {"apple", "banana", "orange", "peach"}
+vegetables = {"tomato", "potato", "onion", "garlic"}
+detergents = {"soap", "shampoo", "conditioner", "surf"}
+grocery = {"sugar", "tea", "chili", "dals"}
