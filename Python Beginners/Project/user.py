8cd22e0437f787a28b3a528a56e98e278db8824c
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2024-08-16 08:54:14 +0500

    Sign up done but backend details

--- Python Beginners/Project/user.py
+++ Python Beginners/Project/user.py
@@ -1,6 +1,25 @@
 def user_login():
     try:
         print('Welcome to the online mart')
-        select2 = input('Select from the following: \n 1.Sign up \n 2.Sign in \n >>> ')
+        select2 = input('Select from the following: \n 1.Sign up \n 2.Sign in \n >>> ').capitalize()
+        if select2 == '1':
+            sign_up()
+        else:
+            sign_in()
     except Exception as e:
         print(f"An error occurred: {e}")
+
+def sign_up():
+    print('---------- Please enter the sign up details ----------')
+    user_name = input('Enter your name: ')
+    user_number = input('Enter your number: +92 ')
+    user_email = input('Enter your email address: ')
+    user_password = input('Enter your password: ')
+    print('---------- Account Created Successfully ----------')
+    return user_name, user_email, user_number, user_password
+
+def sign_in():
+    print('---------- Please enter the login details ----------')
+    user_email = input('Enter your email address: ')
+    user_password = input('Enter your password: ')
+    return user_email, user_password
