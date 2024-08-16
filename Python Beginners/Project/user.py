ae733eb497da4ddfa236ede6028bc8d05dfb2cfb
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2024-08-16 11:37:31 +0500

    User login part is completed

--- Python Beginners/Project/user.py
+++ Python Beginners/Project/user.py
@@ -3,11 +3,17 @@ def user_login():
         print('Welcome to the online mart')
         select2 = input('Select from the following: \n 1.Sign up \n 2.Sign in \n >>> ').capitalize()
         if select2 == '1':
-            sign_up()
+            a, b, c, d = sign_up()
+            if checking(a, b, c, d):
+                print('Account Created Successfully')
+            else:
+                print('Email already exist, please try login')
+                sign_in()
         else:
             sign_in()
     except Exception as e:
         print(f"An error occurred: {e}")
+    return database
 
 def sign_up():
     print('---------- Please enter the sign up details ----------')
@@ -23,3 +29,21 @@ def sign_in():
     user_email = input('Enter your email address: ')
     user_password = input('Enter your password: ')
     return user_email, user_password
+
+def database(name, number, email, password):
+    user_data = []
+    user_data.append(name)
+    user_data.append(number)
+    user_data.append(email)
+    user_data.append(password)
+    return user_data
+
+dataset = []
+
+def checking(name, number, email, password):
+    global dataset
+    for data in dataset:
+        if email == data[2]:
+            return False
+    dataset.append(database(name, number, email, password))
+    return True
